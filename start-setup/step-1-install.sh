742274d546bf141549e43069abad3c73a10d9fc8
Ashim Jung Khadka <ashim.jung.khadka@gmail.com>
2021-12-27 18:04:02 +0545

    fix: add sshfs install for telepresence

--- start-setup/step-1-install.sh
+++ start-setup/step-1-install.sh
@@ -158,6 +158,7 @@ echo "\n\nInstalling Telepresence"
 echo "--------------------------------------"
 sudo curl -fL https://app.getambassador.io/download/tel2/linux/amd64/latest/telepresence -o /usr/local/bin/telepresence
 sudo chmod a+x /usr/local/bin/telepresence
+sudo apt install -y sshfs
 
 
 echo "\n\nInstalling Python"
